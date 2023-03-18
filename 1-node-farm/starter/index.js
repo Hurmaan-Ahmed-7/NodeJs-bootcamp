0fa5a3df36bd3d543bfc50be4967c902f200f997
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-03-18 11:39:02 +0530

    practised routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const http = require('http');
-////////////////////////////////
+////////////////////////////////---
 
 // //synchronous reading and writing
 
@@ -10,7 +10,8 @@ const http = require('http');
 // const writeOutput = `${input} created on ${Date.now()}`;
 // fs.writeFileSync('./txt/output.txt', writeOutput);
 // console.log('file written');
-////////////////////////////////
+
+////////////////////////////////---
 
 // //async reading and writing
 
@@ -19,14 +20,88 @@ const http = require('http');
 // });
 // console.log('reading file async way');
  
-/////////////////////////////////
+/////////////////////////////////---
+
 //creating server
 
+// const serverResult = http.createServer((req, res) => {
+//     res.end("hello");
+// });
+// console.log(serverResult);
+
+
+// serverResult.listen(8000, '127.0.0.1', ()=>{
+//     console.log('listening');
+    
+// });
+
+///////////////////////////////////---
+
+//routing, based on response object, we map the right controller functions.
+
+// const serverResult = http.createServer((req, res) => {
+//     const reqUrl = req.url;
+//     console.log(reqUrl);
+    
+//     if(reqUrl === '/'){
+//         res.end('main page')
+//     }
+//     else if(reqUrl === '/cart'){
+//         res.end('this is your cart');
+//     }
+//     else if(reqUrl === '/item'){
+//         res.end('this is your item');
+//     }
+//     else{
+//         res.writeHead(404, {
+//             'content-type' : 'text/html'
+//         });
+//         res.end('<h1>invalid url</h1>')
+//     }
+// });
+// console.log(serverResult);
+
+
+// serverResult.listen(8000, '127.0.0.1', ()=>{
+//     console.log('listening');
+    
+// });
+
+////////////////////////////////---
+
+//node-farm example
+const dataForApi = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataForApiObj = JSON.parse(dataForApi);
+
 const serverResult = http.createServer((req, res) => {
-    res.end("hello");
+    const reqUrl = req.url;
+    console.log(reqUrl);
+    
+    if(reqUrl === '/'){
+        res.end('main page')
+    }
+    else if(reqUrl === '/cart'){
+        res.end('this is your cart');
+    }
+    else if(reqUrl === '/item'){
+        res.end('this is your item');
+    }
+    else if(reqUrl === '/api'){
+        res.writeHead(200, {
+            'Content-Type' : 'application/json'
+        });
+        res.end(dataForApi);
+    }
+    else{
+        res.writeHead(404, {
+            'Content-Type' : 'text/html'
+        });
+        res.end('<h1>invalid url</h1>')
+    }
 });
+console.log(serverResult);
+
 
 serverResult.listen(8000, '127.0.0.1', ()=>{
     console.log('listening');
-    
 });
