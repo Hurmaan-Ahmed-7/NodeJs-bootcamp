9c424161a866c1a74c386f4a3e1ea7a0b4390a40
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-03-19 15:14:09 +0530

    server side initial document rendering before sending response

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -69,16 +69,136 @@ const http = require('http');
 
 ////////////////////////////////---
 
-//node-farm example
+//node-farm routing 
+
+// const dataForApi = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// const dataForApiObj = JSON.parse(dataForApi);
+
+// const serverResult = http.createServer((req, res) => {
+//     const reqUrl = req.url;
+//     console.log(reqUrl);
+    
+//     if(reqUrl === '/'){
+//         res.end('main page')
+//     }
+//     else if(reqUrl === '/cart'){
+//         res.end('this is your cart');
+//     }
+//     else if(reqUrl === '/item'){
+//         res.end('this is your item');
+//     }
+//     else if(reqUrl === '/api'){
+//         res.writeHead(200, {
+//             'Content-Type' : 'application/json'
+//         });
+//         res.end(dataForApi);
+//     }
+//     else{
+//         res.writeHead(404, {
+//             'Content-Type' : 'text/html'
+//         });
+//         res.end('<h1>invalid url</h1>')
+//     }
+// });
+// console.log(serverResult);
+
+
+// serverResult.listen(8000, '127.0.0.1', ()=>{
+//     console.log('listening');
+// });
+
+// ////////////////////////////////---
+
+// //sending html on req 
+
+// const dataForApi = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// const dataForApiObj = JSON.parse(dataForApi);
+// const overview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+// console.log(overview);
+
+
+// const serverResult = http.createServer((req, res) => {
+//     const reqUrl = req.url;
+//     console.log(reqUrl);
+    
+//     if(reqUrl === '/'){
+//         res.writeHead(200, {
+//             'Content-Type' : 'text/html'
+//         });
+//         res.end(overview);
+//     }
+//     else if(reqUrl === '/cart'){
+//         res.end('this is your cart');
+//     }
+//     else if(reqUrl === '/item'){
+//         res.end('this is your item');
+//     }
+//     else if(reqUrl === '/api'){
+//         res.writeHead(200, {
+//             'Content-Type' : 'application/json'
+//         });
+//         res.end(dataForApi);
+//     }
+//     else{
+//         res.writeHead(404, {
+//             'Content-Type' : 'text/html'
+//         });
+//         res.end('<h1>invalid url</h1>')
+//     }
+// });
+// console.log(serverResult);
+
+
+// serverResult.listen(8000, '127.0.0.1', ()=>{
+//     console.log('listening');
+// });
+
+////////////////////////////////---
+
+//injecting html into document on the server
+//server side rendering USING json data
+
+
 const dataForApi = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataForApiObj = JSON.parse(dataForApi);
+const overviewTemplate = fs.readFileSync(`${__dirname}/templates/overview-template.html`, 'utf-8');
+let cardTemplate = fs.readFileSync(`${__dirname}/templates/card-template.html`, 'utf-8');
+
+
+function serverHtmlRender(el){
+    
+    let output = cardTemplate.replace(/{%IMAGE%}/g, el.image);
+
+    output = output.replace(/{%PRODUCT_NAME%}/g, el.productName);
+
+    output = output.replace(/{%QUANTITY%}/g, el.quantity);
+
+    output = output.replace(/{%PRICE%}/g, el.price);
+
+    if(el.organic){
+        output = output.replace(/{%ORGANIC%}/g,  'ORGANIC');
+    }
+    return output; 
+}
+
+const injectHtml = dataForApiObj.map(el => 
+    serverHtmlRender(el)
+).join('');
+
+// console.log(injectHtml);
+const Output = overviewTemplate.replace('{%INJECT%}', injectHtml);
+// console.log(Output);
 
 const serverResult = http.createServer((req, res) => {
     const reqUrl = req.url;
     console.log(reqUrl);
     
     if(reqUrl === '/'){
-        res.end('main page')
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'
+        });
+        
+        res.end(Output);
     }
     else if(reqUrl === '/cart'){
         res.end('this is your cart');
