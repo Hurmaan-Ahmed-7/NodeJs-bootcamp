3495ca91f4e43e324f0771616377ab2351acfe03
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-03-27 13:41:40 +0530

    built basic natours api- no middleware- functionalities within the api layer

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -59,9 +59,23 @@ app.get('/api/v1/tours', (req, res) => {
 //request
 
 app.post('/api/v1/tours', (req, res) => {
-  req.
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({id: newId}, req.body);
+  tours.push(newTour);
+  fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours), err => {
+    res
+    .status(201)
+    .json({
+        status: 'success',
+        data: {
+            tour: newTour 
+        }
+    })
+  });
 });
 const port = 3000;
 app.listen(port, (req, res) =>{
   console.log('listening on port: ' + port);
 });
+/////////////////////////////////////
+//handling url params
