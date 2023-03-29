41b88e5727662099359be8fb4326a7516dd95278
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-03-29 07:17:27 +0530

    added database to the application

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+
 const app = express();
 const morgan = require('morgan');
-const fs = require('fs');
+
 //importing routers
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -9,138 +10,17 @@ const userRouter = require('./routes/userRoutes');
 app.use(morgan('dev'));
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log('Hello from the middleware 👋');
-    next();
-});  
+  //console.log('Hello from the middleware 👋');
+  next();
+});
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 //routers used to define express object used as middleware
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
-/////////////////////////////////////
-//routing get requests using express.
-
-// console.log(app);
-// console.log(express);
-
-// const port = 3000;
-//  app.listen(port, () => {
-//   console.log('port is on');
-  
-//  });
-
-//  app.get('/', (req, res) =>{
-//   res
-//   .status(200)
-//   .json({message: 'Hello from main'});
-
-// });
-//  app.get('/about', (req, res) =>{
-//   res.send('hello to about');
-// });
-//  app.get('/exir', (req, res) =>{
-//   res.send('hello to exit');
-// });
-// app.post('/', (req, res)=>{
-//   res.send('you can post here')
-//   .status(200);
-// });
-// app.use((req, res) => {
-//   res.status(404).send('404 not found')
-// });
-
-////////////////////////////////////
-//building the natours api using REST
-// //sending json data to the client
-// //upon request.
-
-// app.get('/api/v1/tours', (req, res) => {
-//   res
-//   .status(200)
-//   .json({
-//     status: 'success',
-//     data: {
-//       tours: tours
-//     }
-//   })
-// });
-
-// //accepting json data from the client
-// //and writing it to the file upon client
-// //request
-
-// app.post('/api/v1/tours', (req, res) => {
-//   const newId = tours[tours.length - 1].id + 1;
-//   const newTour = Object.assign({id: newId}, req.body);
-//   tours.push(newTour);
-//   fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours), err => {
-//     res
-//     .status(201)
-//     .json({
-//         status: 'success',
-//         data: {
-//             tour: newTour 
-//         }
-//     })
-//   });
-// });
-
-// /////////////////////////////////////
-// //handling url params
-
-// app.get('/api/v1/tours/:id', (req, res) => {
-//     const id = parseInt(req.params.id, 10);
-    
-//     const tour = tours.find(function (el){
-        
-//         if(el.id == id){
-//             // console.log(el.id);
-//             return true;
-//         }
-//     });
-//     if(!tour){
-//         res
-//         .status(404)
-//         .json({
-//             status: '404 not found',
-//             data: {
-//                 tour
-//             }
-//         });
-//     }
-//     else{
-//     res
-//     .status(200)
-//     .json({
-//         status: 'success',
-//         data: {
-//             tour
-//         }
-//     });
-//     }
-    
-// });
-// /////////////////////////////////////
-// //handling patch requests
-
-// app.patch('/api/v1/tours/:id', (req, res) => {
-//     const id = parseInt(req.params.id, 10);
-//     const tour = tours.find(function (el){
-        
-//         if(el.id == id){
-//             // console.log(el.id);
-//             return true;
-//         }
-//     }); 
-//     //not implemented yet
-//     //incomplete......
-
-// });
-
 
 /////////////////////////////////////
 
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,21 @@
+require('dotenv').config(
+  { path: './config.env'}
+);
+const mongoose = require('mongoose');
 const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('db connected successfully');
+  });
+
 //listening on localhost
 const port = 3000;
 app.listen(port, (req, res) => {
-  console.log('listening on port: ' + port);
+  console.log(`listening on port: ${port}`);
 });
