3360e8d81f0580b0612fb6e708971df27d19ecfc
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-03-31 08:12:28 +0530

    add query using url functionality

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,26 +1,50 @@
 const fs = require('fs');
+const { stringify } = require('querystring');
 const Tour = require('./../models/tourModel');
 //route handlers
 const getTours = async (req, res) => {
     try{
-        const tours = await Tour.find();
-        res
-        .status(200)
-        .json({
-          status: 'success',
-          requestedAt: req.requestTime,
-          data: {
-            tours: tours
-          }
-        })
+      // console.log(req.query);
+      //hard storing the query from url
+      let urlQueryObj = {...req.query};
+      //formatting the hard copy to fit the parameter spec for mongoose methods
+      excludeFields = ['sort', 'page', 'limit', 'fields'];
+      excludeFields.forEach(value => delete urlQueryObj[value]);
+      // console.log(urlQueryObj);
+      queryStr = JSON.stringify(urlQueryObj);
+      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match =>`$${match}`);
+      urlQueryObj = JSON.parse(queryStr);
+      // console.log(urlQueryObj);
+      //query using the formatted hard copy
+      let queryObj = Tour.find(urlQueryObj);
+      //receives object of type query since await is not used here
+      //we use the OG req.query here not the hard copy
+      if(req.query.sort){
+        const sortVars = req.query.sort.split(',').join(' ');
+        // console.log(sortVars);
+        queryObj = queryObj.sort(sortVars);
+      }
+      const tours = await queryObj;
+
+      console.log(tours);
+      
+      res
+      .status(200)
+      .json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        data: {
+          tours: tours
+        }
+      })
     }
     catch(err){
-        res
-        .status('400')
-        .json({
-          status: 'failed',
-          message: err
-        });
+      res
+      .status('400')
+      .json({
+        status: 'failed',
+        message: err
+      });
     }
 }
 const createTour = async(req, res) => {
@@ -28,49 +52,49 @@ const createTour = async(req, res) => {
         const newTour = await Tour.create(req.body);
 
         res
-          .status(201)
-          .json({
-            status: 'success',
-            data: {
-              tour: newTour 
-            }
-        });
+        .status(201)
+        .json({
+          status: 'success',
+          data: {
+            tour: newTour 
+          }
+      });
     }   
     catch(err){
-        console.log(err);
+      console.log(err);
         
-        res
-          .status('400')
-          .json({
-            status: 'failed',
-            message: err
-          });
+      res
+        .status('400')
+        .json({
+          status: 'failed',
+          message: err
+        });
     }
 }
 const getTourById = async (req, res) => {
-    try{
-        const tour = 
-        await Tour.findById(req.params.id);
+  try{
+      const tour = 
+      await Tour.findById(req.params.id);
 
-        res 
-         .status(200)
-         .json({
-            status: 'success',
-            data: {
-            tour
-            }
+      res 
+      .status(200)
+      .json({
+         status: 'success',
+         data: {
+           tour
+          }
         });
     }
     catch(err){
-        console.log(err);
+      console.log(err);
         
-        res
-          .status('400')
-          .json({
-            status: 'failed',
-            message: err
-          });
-    }
+      res
+      .status('400')
+      .json({
+        status: 'failed',
+        message: err
+      });
+    } 
 
 }
 const updateTourById = async (req, res) => {
@@ -82,23 +106,23 @@ const updateTourById = async (req, res) => {
         });
 
         res 
-         .status(200)
-         .json({
-            status: 'success',
-            data: {
-            tour
-            }
+        .status(200)
+        .json({
+          status: 'success',
+          data: {
+          tour
+          }
         });
-    }
+      }
     catch(err){
-        console.log(err);
+      console.log(err);
         
-        res
-          .status('400')
-          .json({
-            status: 'failed',
-            message: err
-          });
+      res
+        .status('400')
+        .json({
+          status: 'failed',
+          message: err
+        });
     }
 }
 const deleteTourById = async (req, res) => {
