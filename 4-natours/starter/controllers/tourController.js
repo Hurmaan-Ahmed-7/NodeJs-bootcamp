2779f19f210e0c5ce5caf55a41fcb850e6827fba
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-03 13:50:04 +0530

    refractored the code- create utils directory

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,62 +1,5 @@
-const { clear } = require('console');
-const fs = require('fs');
-const { stringify } = require('querystring');
 const Tour = require('./../models/tourModel');
-class ApiQueryFeatures{
-  constructor(queryObj, urlQueryObj){
-    this.queryObj = queryObj;
-    this.urlQueryObj = urlQueryObj;
-  }
-  filter(){
-    // console.log('filtering...');
-    //hard storing the query from url
-    let urlQueryObj = {...this.urlQueryObj};
-    //formatting the hard copy to fit the parameter spec for mongoose methods
-    const excludeFields = ['sort', 'page', 'limit', 'fields'];
-    excludeFields.forEach(value => delete urlQueryObj[value]);
-    let queryStr = JSON.stringify(urlQueryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match =>`$${match}`);
-    urlQueryObj = JSON.parse(queryStr);
-    //filter using the formatted hard copy
-    //receives object of type query since await is not used here
-    this.queryObj = Tour.find(urlQueryObj);
-    //for method chaining
-    return this;
-  }
-  sorting(){
-    if(this.urlQueryObj.sort){
-    // console.log('sorting...');
-    // console.log(this.urlQueryObj);
-    const sortVars = this.urlQueryObj.sort.split(',').join(' ');
-    this.queryObj = this.queryObj.sort(sortVars);
-    // console.log('sorting end');
-    }
-    return this;
-  }
-  fielding(){
-    if(this.urlQueryObj.fields){
-    // console.log('fielding...');
-    const fields = this.urlQueryObj.fields.split(',').join(' ');
-    this.queryObj = this.queryObj.select(fields);
-    }
-    return this;
-  }
-  async pagination(){
-    if(this.urlQueryObj.page){
-    // console.log('paginating...');
-    const page = this.urlQueryObj.page * 1 || 1;
-    const limit = this.urlQueryObj.limit * 1 || 30;
-    const skip = (page - 1) * limit;
-    this.queryObj = this.queryObj.skip(skip).limit(limit);
-    
-    const numOfTours = await Tour.countDocuments();
-    if(skip >= numOfTours){
-        throw new Error('no more data at this page');
-    }
-    }
-    return this;
-  }
-}
+const ApiQueryFeatures = require('./../utils/apiFeatures.js');
 
 //alias'
 const top5CheapAlias = async (req, res, next) => {
@@ -69,11 +12,17 @@ const top5CheapAlias = async (req, res, next) => {
 const getTours = async (req, res) => {
     try{
       const features = new ApiQueryFeatures(Tour.find(), req.query);
-      features
-        .filter()
-        .sorting()
-        .fielding()
-        .pagination();
+      // features
+      //   .filter()
+      //   .sorting()
+      //   .fielding()
+      //   .pagination();
+      console.log(features);
+      console.log(features.filter());
+      console.log(features.sorting());
+      console.log(features.fielding());
+      console.log(features.pagination());
+      
       //await here waits for the exec()method which turns the query object into array of documents
       const tours = await features.queryObj;
       
