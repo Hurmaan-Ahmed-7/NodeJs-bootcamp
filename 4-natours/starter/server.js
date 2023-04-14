1d92be28ef8f19b521adc75a239fd34f6a2ddcaf
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-14 13:11:55 +0530

    add error handling for unhandled rejections

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -11,13 +11,22 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  })
+  }) 
   .then(() => {
     console.log('-------db connected successfully--------');
     console.log(`running on ${process.env.NODE_ENV} environment.......`);
   });
+
 //listening on localhost
-const port = 3000;
-app.listen(port, (req, res) => {
+const port = process.env.PORT || 3000;
+const serverOn = app.listen(port, (req, res) => {
   console.log(`listening on port: ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('unhandled rejection, shutting down');
+  serverOn.close(() => {
+    process.exit(1);
+  });
 });
