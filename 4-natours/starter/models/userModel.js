9e5ee5e9bd9863df3fdba346d31032f52930f404
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-14 15:04:04 +0530

    create user schema

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A user must have a name'],
+        unique: true,
+        trim: true,
+        maxlength: [40, 'A user name must have less or equal then 40 characters'],
+        minlength: [10, 'A user name must have more or equal then 10 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'A user must have an email'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'A user must have a password'],
+        minlength: [8, 'A user password must have more or equal then 8 characters'],
+        select: false
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'A user must confirm his password'],
+    }
+});
