51501fed9d9f4c2501fbcb99b5ea7f3bb33ffd7c
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-18 21:13:34 +0530

    http safe features

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,16 +1,31 @@
 const express = require('express');
-
 const app = express();
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const errorHandler = require('./controllers/errorHandler');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 //importing routers to be used as middleware
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-//add random middleware's
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(morgan('dev'));
+//add random global middleware's
+app.use(helmet());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+//middlewares for http security
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour'
+}); 
+app.use('/api', limiter);
+app.use(express.json({ limit: '10kb'}));
+app.use(mongoSanitize());
+app.use(xss());
+app.use(express.static(`${__dirname}/public`));
 //add routers as middleware's
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
