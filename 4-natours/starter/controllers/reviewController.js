2399f57c2ed4b2039f8282d9ac8d86a33845519f
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-25 13:30:22 +0530

    add nested routes

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -18,10 +18,13 @@ const getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 const createReview = catchAsync(async (req, res, next) => {
+    console.log('req.body: ', req.body);
+    
     //Allow nested routes
     if(!req.body.tour) req.body.tour = req.params.tourId;
     if(!req.body.user) req.body.user = req.user.id;
-
+    console.log(req.params, req.body.tour, req.body.user);
+    
     const newReview = await Review.create(req.body);
     if (!newReview) {
         return next(new AppError('No review found with that ID', 404));
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const tourController = require(`./../controllers/tourController`);
 authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 //alias router
 router
   .route('/top-5-cheap')
   .get(tourController.top5CheapAlias, tourController.getTours)
   .post(tourController.createTour);
 //resource routers
+router.use('/:tourId/reviews', reviewRouter);
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
@@ -23,13 +24,6 @@ router
     authController.protect,
     authController.restrict('admin', 'lead-guide'),
     tourController.deleteTourById
-  );
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrict('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
