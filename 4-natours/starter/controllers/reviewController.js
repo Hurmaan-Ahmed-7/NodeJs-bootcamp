ad8fbe7bb467cd81d6d94c45b5c2861adc552a95
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-04-25 13:42:06 +0530

    add nested route to get all reviews of a tour

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -3,41 +3,45 @@ const AppError = require('./../utils/appError');
 const Review = require('./../models/reviewModel');
 
 const getAllReviews = catchAsync(async (req, res, next) => {
-    const reviews = await Review.find();
-    if (!reviews) {
-        return next(new AppError('No reviews yet!', 404));
+  let filter = {};
+  if (req.params.tourId) {
+    filter = { tour: req.params.tourId };
+  }
+  const reviews = await Review.find(filter);
+  if (!reviews) {
+    return next(new AppError('No reviews yet!', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    requestedAt: req.requestTime,
+    data: {
+      reviews
     }
-    res.status(200).json({
-        status: 'success',
-        results: reviews.length,
-        requestedAt: req.requestTime,
-        data: {
-            reviews
-        }
-    });
+  });
 });
 
 const createReview = catchAsync(async (req, res, next) => {
-    console.log('req.body: ', req.body);
-    
-    //Allow nested routes
-    if(!req.body.tour) req.body.tour = req.params.tourId;
-    if(!req.body.user) req.body.user = req.user.id;
-    console.log(req.params, req.body.tour, req.body.user);
-    
-    const newReview = await Review.create(req.body);
-    if (!newReview) {
-        return next(new AppError('No review found with that ID', 404));
+  console.log('req.body: ', req.body);
+
+  //Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  console.log(req.params, req.body.tour, req.body.user);
+
+  const newReview = await Review.create(req.body);
+  if (!newReview) {
+    return next(new AppError('No review found with that ID', 404));
+  }
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview
     }
-    res.status(201).json({
-        status: 'success',
-        data: {
-            review: newReview
-        }
-    });
+  });
 });
 
 module.exports = {
-    getAllReviews,
-    createReview
+  getAllReviews,
+  createReview
 };
